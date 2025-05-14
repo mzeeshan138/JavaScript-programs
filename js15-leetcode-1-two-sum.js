6d4da38fd9ae399355dfbdca748fb589d2b75cd2
mzeeshan138 <muhammadzeeshan786138@gmail.com>
2025-05-14 19:59:35 +0500

    js15-leetcode-1-two-sum.js

---
+++ js15-leetcode-1-two-sum.js
@@ -0,0 +1,14 @@
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] === target - nums[i]) {
+        return [i, j];
+      }
+    }
+  }
+  // Return an empty array if no solution is found
+  return [];
+};
+
+console.log(twoSum([2, 7, 11, 15], 9)); //[0,1]
+console.log(twoSum([3, 2, 4], 6)); //[1,2]
